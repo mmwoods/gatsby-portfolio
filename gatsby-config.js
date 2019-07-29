b171a71a0b559d5a89ca6e3ac79fc8d047640273
Mitchell Woods <themitchwoods@gmail.com>
2019-07-29 13:17:26 +1000

    Add css-modules typescript support

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-transformer-typescript-css-modules`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
