8873bf8909006082da14aa344a584f99eef198a7
Mitchell Woods <mw@mitchellmwoods.com>
2019-08-05 21:54:12 +1000

    #4 Added contact data

---
+++ src/data/contact.ts
@@ -0,0 +1,10 @@
+export const contact = {
+  name: "Mitchell Woods",
+  title: "Front End Developer/Designer",
+  location: "Brisbane, QLD, 4000",
+  phone: "0409463422",
+  email: "mw@mitchellmwoods.com",
+  linkedin: "mitchellmwoods",
+  github: "mmwoods",
+  website: "mitchellmwoods",
+}
