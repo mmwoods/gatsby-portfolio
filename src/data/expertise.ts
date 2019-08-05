73ca01316d3534fb2e7983c2d11e0639028a98d9
Mitchell Woods <mw@mitchellmwoods.com>
2019-08-05 21:12:06 +1000

    Added expertise data

---
+++ src/data/expertise.ts
@@ -0,0 +1,46 @@
+export const expertise = [
+  {
+    category: "HTML5/CSS3",
+    list: ["SASS/Less", "Bootstrap 3/4", "CSS Modules"],
+  },
+  {
+    category: "JavaScript/TypeScript",
+    list: ["ES6", "React", "Electron"],
+  },
+  {
+    category: "UI/Design",
+    list: ["Figma", "Illustrator", "Storybook"],
+  },
+  {
+    category: "PHP",
+    list: ["Craft CMS", "Twig", "Mustache"],
+  },
+  {
+    category: "Server-Side",
+    list: ["Node"],
+  },
+  {
+    category: "Version Control",
+    list: ["Git", "GitHub", "Bitbucket"],
+  },
+  {
+    category: "Build Process",
+    list: ["BuildKite", "Gulp"],
+  },
+  {
+    category: "eCommerce",
+    list: ["Neto (B@SE)", "Shopify"],
+  },
+  {
+    category: "Project Management",
+    list: ["Confluence", "Jira", "Scrum"],
+  },
+  {
+    category: "CMS",
+    list: ["Salesforce", "Contentful", "Squiz", "Craft"],
+  },
+  {
+    category: "Other",
+    list: ["Regex", "Netlify", "Percy", "Lerna"],
+  },
+]
